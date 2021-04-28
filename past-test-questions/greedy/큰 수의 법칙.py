f9142965858da25847c5ec6a1634a7691f546cd7
0general <0general@naver.com>
2021-04-28 15:53:25 +0900

    greedy - 큰 수의 법칙 (1)

--- past-test-questions/greedy/큰 수의 법칙.py
+++ past-test-questions/greedy/큰 수의 법칙.py
@@ -17,17 +17,20 @@ K<=M
 K는 특정한 인덱스의 수가 연속으로 더해질 수 있는 숫자를 의미한다. 
 '''
 
-n, m, k = map(int, input().split())
+n, m, k = map(int, input('n과 m과 k를 입력하시오 : ').split())
 
-data = list(map(int, input().split()))
+while 1:
+    data = list(map(int, input('n 만큼의 배열을 입력하시오 : ').split()))
+    if len(data) == n: break;
 
 data.sort(reverse=True)
+print("가장 큰수 : ", data[0])
+print("두번째로 큰 수 : ", data[1]) 
 
-count = int(m/(k+1))
-count += m%(k+1)
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
 
-result = data[0]*count
-result += data[0]*(m-count)
+result = data[0] * count
+result += data[1] * (m - count)
 
 print(result)
-
