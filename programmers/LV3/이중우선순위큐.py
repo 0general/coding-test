39104ffdeef4d97fbb5a9107f8532508396170b9
0general <0general@naver.com>
2021-08-19 10:17:34 +0900

    heapq이용하는 방법 연구하기

--- programmers/LV3/이중우선순위큐.py
+++ programmers/LV3/이중우선순위큐.py
@@ -14,3 +14,29 @@ def solution(operations):
                 answer = answer[1:]
 
     return [answer[-1], answer[0]] if len(answer) != 0 else [0, 0]
+
+
+
+'''
+import heapq
+
+def solution(operations):
+    h = []
+
+    for i in operations:
+        a, b = i.split()
+        if a == 'I':
+            heapq.heappush(h, int(b))
+        else:
+            if len(h) > 0:
+                if b == '1':
+                    h.pop(h.index(heapq.nlargest(1, h)[0]))
+                else:
+                    heapq.heappop(h)
+
+    if len(h) == 0:
+        return [0, 0]
+    else:
+        return [heapq.nlargest(1, h)[0], h[0]]
+
+'''
