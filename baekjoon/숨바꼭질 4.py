b55e09a7ef538e191a72016e279ac79ff3b42cdc
0general <0general@naver.com>
2021-09-30 23:19:01 +0900

    수정 완료 코드 조건문의 위치가 달라지면 결과도 달라진다.

--- baekjoon/숨바꼭질 4.py
+++ baekjoon/숨바꼭질 4.py
@@ -3,39 +3,36 @@ https://www.acmicpc.net/problem/13913
 """
 import sys
 from collections import deque
+
 input = sys.stdin.readline
-
 n, k = map(int, input().split())
-
-if n == k:
-    print(0)
-    sys.exit()
 
 map = [0]*100001
 prev = [0]*100001
 
 queue = deque()
+ans = []
 
 cnt = 0
 map[n] = cnt
 prev[n] = -1
 queue.append(n)  # 인덱스
-ans = []
 
 while queue:
     now = queue.popleft()
-    cnt = map[now]
+    if now == k:
+        print(map[now])
+        ans.append(str(now))
+        s = prev[now]
+        while s != -1:
+            ans.append(str(s))
+            s = prev[s]
+        ans.reverse()
+        print(" ".join(ans))
+        break
+
     for next in (now-1, now+1, 2*now):  # 다음 인덱스
         if 0 <= next < 100001 and map[next] == 0 and next != n:
-            map[next] = cnt+1
+            map[next] = map[now]+1
             prev[next] = now
             queue.append(next)
-        if next == k:
-            ans.append(str(k))
-            s = prev[k]
-            while s != -1:
-                ans.append(str(s))
-                s = prev[s]
-            ans.reverse()
-            print(" ".join(ans))
-            sys.exit()
