534cb797fc7b94e0067a133b0abf0b168d0353ef
0general <0general@naver.com>
2021-10-07 16:02:10 +0900

    분할 정복 recursion 깊이를 충분히 확보하고 인덱스를 이용하여 가능한 메모리를 줄이고 시간 초과를 막기 위해 PyPy3로 채점

--- baekjoon/트리의 순회.py
+++ baekjoon/트리의 순회.py
@@ -3,12 +3,36 @@ https://www.acmicpc.net/problem/2263
 """
 import sys
 
+
+def find_index(si, ei, v):
+    for i in range(ei-si+1):
+        if vin[si+i] == v:
+            return i
+
+
+def find_root(si, ei, ps, pe):  # 인오더, 포스트오더
+    if si > ei:
+        return
+    if si == ei:
+        print(vin[si], end=' ')
+        return
+    v = post[pe]
+    print(v, end=' ')
+
+    ix = find_index(si, ei, v)
+    find_root(si, si+ix-1, ps, ps+ix-1)
+    find_root(si+ix+1, ei, ps+ix, pe-1)
+
+
 input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
 
 n = int(input())
 
-inorder = list(map(int, input().split()))
-postorder = list(map(int, input().split()))
+vin = list(map(int, input().split()))
+post = list(map(int, input().split()))
 
-num = len(inorder)
-preorder = [0] * num
+
+s = 0
+e = len(vin)-1
+find_root(s, e, s, e)
