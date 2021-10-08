4dd4d944b3e7a1c8500b078f0f04ce6fe4474b7b
0general <0general@naver.com>
2021-10-08 18:58:09 +0900

    브루트 포스, 이분탐색 meet in the middle 전체 그룹을 둘로 나눠서 각자 합을 구한 뒤 한 그룹에 한 값을 선택, 다른 그룹에서 그 값을 빼고도 남은 가치를 만족하는 모든 개수 구하기

--- baekjoon/냅색 문제.py
+++ baekjoon/냅색 문제.py
@@ -1,12 +1,20 @@
 '''
 https://www.acmicpc.net/problem/1450
 '''
-"""
-https://www.acmicpc.net/problem/1450
-"""
-
+#from itertools import combinations
+'''
 import sys
 input = sys.stdin.readline
+
+
+def bruteforce(sum, goods, id, w):
+    if id >= len(goods):
+        if w <= c:
+            sum.append(w)
+        return
+    bruteforce(sum, goods, id+1, w)
+    bruteforce(sum, goods, id+1, w + goods[id])
+
 
 # n개의 물건, 넣을 수 있는 최대 c의 무게
 n, c = map(int, input().split())
@@ -16,17 +24,65 @@ goods.sort()
 dp = [0] * (c+1)  # 인덱스 : 무게, 값 : 개수
 dp[0] = 1
 l, r = 0, n-1
-ans = 0
 while l < r:
     mid = (l+r)//2
     if goods[mid] <= c:
-        ans = mid
         l = mid + 1
     else:
-        r = mid - 1
+        r = mid
+goods = goods[:r+1]
 
-for good in goods[:ans+1]:
-    dp[good] += 1
+ans = len(goods)
+
+for i in range(2, len(goods)+1):
+    for check in combinations(goods, i):
+        if sum(check) <= c:
+            ans += 1
+
+print(ans+1)
+메모리 초과
+
+# sum = []
+# bruteforce(sum, goods, 0, 0)
+# print(len(sum))
+이것도 메모리 초과
+'''
+input = sys.stdin.readline
+
+n, c = map(int, input().split())
+weight = list(map(int, input().split()))
+aw = weight[:n//2]
+bw = weight[n//2:]
+asum = []
+bsum = []
 
 
-print(sum(dp[:ans+1]))
+def bruteforce(warr, sum, l, w):
+    if l >= len(warr):
+        sum.append(w)
+        return
+    bruteforce(warr, sum, l+1, w)
+    bruteforce(warr, sum, l+1, w+warr[l])
+
+
+def binarySearch(arr, target, start, end):
+    while start < end:
+        mid = (start + end)//2
+        if arr[mid] <= target:
+            start = mid + 1
+        else:
+            end = mid
+    return end
+
+
+bruteforce(aw, asum, 0, 0)
+bruteforce(bw, bsum, 0, 0)
+bsum.sort()
+
+cnt = 0
+for i in asum:
+    if c-i < 0:
+        continue
+    # c-i보다 작거나 같은 수의 인덱스 = 가능한 모든 수들의 개수
+    cnt += binarySearch(bsum, c-i, 0, len(bsum))
+print(cnt)
