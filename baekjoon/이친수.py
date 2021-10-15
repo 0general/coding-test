e922614553449c1042fee99b4728a54cf50be6ee
0general <0general@naver.com>
2021-10-15 18:22:51 +0900

    기본 dp

---
+++ baekjoon/이친수.py
@@ -0,0 +1,15 @@
+"""
+https://www.acmicpc.net/problem/2193
+"""
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[0, 0] for _ in range(n+1)]
+
+dp[1] = [0, 1]
+
+for i in range(2, n+1):
+    dp[i] = [sum(dp[i-1]), dp[i-1][0]]
+
+print(sum(dp[n]))
