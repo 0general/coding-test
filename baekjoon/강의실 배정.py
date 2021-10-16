c3b849df7fa3330bba0dad40848b1553fd739566
0general <0general@naver.com>
2021-10-16 22:58:25 +0900

    동일한 로직이지만 차이점에서 발생하는 문제가 뭔지 생각해볼 것.

---
+++ baekjoon/강의실 배정.py
@@ -0,0 +1,31 @@
+"""
+https://www.acmicpc.net/problem/11000
+"""
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+h = []
+for _ in range(n):
+    s, t = map(int, input().split())
+    heapq.heappush(h, (s, t))
+
+full = []
+heapq.heappush(full, heapq.heappop(h)[1])
+ans = 1
+while h:
+    if len(full) == 0:
+        heapq.heappush(full, heapq.heappop(h)[1])
+        continue
+    # s, t = heapq.heappop(h)
+    # if s < full[0]:
+    #     heapq.heappush(full, t) 이렇게 쓰면 틀렸다고 한다. 왜지? 같은 로직 아닌가?
+    if full[0] > h[0][0]:
+        heapq.heappush(full, heapq.heappop(h)[1])
+        if len(full) > ans:
+            ans += 1
+    else:
+        heapq.heappop(full)
+
+print(ans)
