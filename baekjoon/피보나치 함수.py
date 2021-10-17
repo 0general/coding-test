05faefd86b72b2af0dbf6eceaf81dfd4ad68f9a9
0general <0general@naver.com>
2021-10-17 23:35:51 +0900

    기본 DP

---
+++ baekjoon/피보나치 함수.py
@@ -0,0 +1,16 @@
+"""
+https://www.acmicpc.net/problem/1003
+"""
+import sys
+input = sys.stdin.readline
+
+dp = [[0, 0] for _ in range(41)]
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+for i in range(2, 41):
+    dp[i] = [dp[i-1][0]+dp[i-2][0], dp[i-1][1]+dp[i-2][1]]
+
+t = int(input())
+for _ in range(t):
+    p = int(input())
+    print(" ".join(map(str, dp[p])))
