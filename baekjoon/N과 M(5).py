c17c4d43def74ba459eb8b6595ff7348c07d125b
Hi YoungJin <56211221+0general@users.noreply.github.com>
2021-10-24 17:15:13 +0900

    순열

---
+++ baekjoon/N과 M(5).py
@@ -0,0 +1,19 @@
+def BT(cnt, i, string):
+    if cnt == m:
+        print(string)
+        return
+    for j in range(n):
+        if not visited[j]:
+            visited[j] = True
+            BT(cnt+1, j, string + " " + str(num[j]))
+            visited[j] = False
+
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+visited = [False]*n
+
+for i in range(n):
+    visited[i] = True
+    BT(1, i, str(num[i]))
+    visited[i] = False
