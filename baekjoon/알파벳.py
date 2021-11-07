265fbe8c08cb81f433cd168f81bac2f0e1d57a78
0general <0general@naver.com>
2021-11-07 22:35:10 +0900

    완전 탐색으로 충분히 가능한 길이(최대 26)에서 망하는 건 dfs를 잘못 짜는 것이 아닌 다른 요인이다. list에서 원소 하나에 접근하여 수정하는 것은 O(1)으로 빠르다. dict는 최대 O(n)일 수 있고 set 또한 마냥 빠르지 않으므로 따로 시간 복잡도 확인할 것.

--- baekjoon/알파벳.py
+++ baekjoon/알파벳.py
@@ -9,25 +9,27 @@ def dfs(i, j, num):
     global length
 
     length = max(num, length)
+    if length == 26:
+        return
 
     for k in range(4):
         x = i + dx[k]
         y = j + dy[k]
-        if ((0 <= x < r) and (0 <= y < c)) and graph[x][y] not in al:
-            al.add(graph[x][y])
+        if ((0 <= x < r) and (0 <= y < c)) and not al[ord(graph[x][y])-65]:
+            al[ord(graph[x][y])-65] = 1
             dfs(x, y, num + 1)
-            al.remove(graph[x][y])
+            al[ord(graph[x][y])-65] = 0
 
 
 r, c = map(int, input().split())
 
 graph = [list(input()) for _ in range(r)]
-al = set()
+al = [0]*26
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-al.add(graph[0][0])
+al[ord(graph[0][0])-65] = 1
 length = 1
 dfs(0, 0, length)
 print(length)
@@ -50,4 +52,8 @@ output : 26
 시간 엄청 오래 걸림
 좌표마다 들어갈 수 있는 값을 가지고 있는게 낫다.
 dfs로는 시간 초과
+-> dfs가 아닌 set 사용한 visit 확인이 시간 초과 요인
+
+참고 : https://www.acmicpc.net/board/view/56580 , https://thecodingbot.com/time-complexity-analysis-of-python-dictionarys-get-method/
+
 '''
