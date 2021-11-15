edd1314ca1c31e26c40936a1f6b0aee5ae1f1b79
0general <0general@naver.com>
2021-11-15 20:27:05 +0900

    이분탐색으로 푼 감소하는 부분 수열. dp가 정석이지만 이분탐색으로도 풀 수 있다. python3 채점이 pypy보다 훨씬 적은 메모리와 시간이 걸린다

---
+++ baekjoon/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,35 @@
+"""
+https://www.acmicpc.net/problem/11722
+"""
+import sys
+
+
+def BT(num):  # 내림차순 Binary Search
+    if len(stack) == 0:
+        stack.append(num)
+        return
+    start, end = 0, len(stack)-1
+    ans = len(stack)
+    while start <= end:  # num의 위치 찾기
+        mid = (start+end)//2
+        if stack[mid] <= num:  # num이 크거나 같으면 더 앞쪽으로 가야 함
+            ans = mid
+            end = mid-1
+        else:  # num이 더 작으면 뒤쪽 그룹에서 찾기
+            start = mid + 1
+    if ans == len(stack):
+        stack.append(num)
+    else:
+        stack[ans] = num
+    return
+
+
+input = sys.stdin.readline
+
+n = int(input())
+seq = list(map(int, input().split()))
+stack = []
+for num in seq:
+    BT(num)
+
+print(len(stack))
