69c3f62f71a4b304a7e9882cfdf84d039b93f8ef
0general <0general@naver.com>
2021-11-29 22:32:56 +0900

    이분 탐색 LIS, 시간 복잡도 좀 더 고민해볼 것.

---
+++ baekjoon/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,35 @@
+"""
+https://www.acmicpc.net/problem/12015
+"""
+import sys
+
+
+input = sys.stdin.readline
+
+# 수열의 길이
+n = int(input())
+arr = list(map(int, input().rstrip().split()))
+ls = []
+
+for i in range(n):
+    if len(ls) == 0:
+        ls.append(arr[i])
+    else:
+        if arr[i] > ls[-1]:
+            ls.append(arr[i])
+            continue
+        elif arr[i] == ls[-1]:
+            continue
+        else:
+            ans = len(ls)-1
+            s, e = 0, ans
+            while s <= e:
+                mid = (s+e)//2
+                if ls[mid] >= arr[i]:
+                    ans = mid
+                    e = mid-1
+                else:
+                    s = mid+1
+            ls[ans] = arr[i]
+
+print(len(ls))
