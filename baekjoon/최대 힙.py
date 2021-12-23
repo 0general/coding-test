dde622c9669934926e164e5be5cba4c7a220d0d6
0general <0general@naver.com>
2021-12-23 23:33:54 +0900

    라이브러리 쓰지 말고 힙 구현해서 해볼 것.

---
+++ baekjoon/최대 힙.py
@@ -0,0 +1,20 @@
+"""
+https://www.acmicpc.net/problem/11279
+"""
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+h = []
+for _ in range(N):
+    print(h)
+    x = int(input())
+    if x == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(h))
+    else:
+        heapq.heappush(h, -x)
