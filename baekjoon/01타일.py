ac31dea5fb2da472f09b98790ae8c49cc427726c
0general <0general@naver.com>
2021-12-24 23:34:16 +0900

    기본 DP

---
+++ baekjoon/01타일.py
@@ -0,0 +1,14 @@
+"""
+https://www.acmicpc.net/problem/1904
+"""
+n = int(input())
+
+dp = [0]*(n+1)
+dp[0] = 1
+dp[1] = 1
+if n == 1:
+    print(dp[n])
+else:
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746
+    print(dp[n])
