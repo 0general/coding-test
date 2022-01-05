dec1805357a55f5238bf5b6f7e5a5fc9834c18a9
0general <0general@naver.com>
2022-01-05 23:12:03 +0900

    메모리 초과 이유 찾아냄. visited 명시해주지 않아도 되는 BFS

--- baekjoon/16953 A to B.py
+++ baekjoon/16953 A to B.py
@@ -1,7 +1,7 @@
 """
 https://www.acmicpc.net/problem/16953
 """
-# 기본 BFS 이용시 메모리 초과
+# BFS로도 가능, 아래에 코드 확인
 
 A, B = map(int, input().split())
 # A에서 B를 찾는 문제가 아닌 B에서 A를 찾는 문제로 바꾸자.
@@ -22,3 +22,34 @@ while A <= B:
         print(-1)
         break
     cnt += 1
+
+
+'''
+import sys
+from collections import deque
+
+
+def bfs(a, b):
+    q = deque()
+    q.append((a, 1))
+    while q:
+        n, t = q.popleft()
+        for m in [2*n, int(str(n)+"1")]:
+            if m <= b:
+                if m == b:
+                    return t+1
+                q.append((m, t+1))
+    return -1
+
+
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+print(bfs(a, b))
+
+
+# visited 없어도 됨. 
+# visited = [False]*(b+1) 하나 했다가 메모리 초과 엄청났지만, 
+# 그거 없애니까 통과됨.
+'''
