afdba52ff3d0cf16c0a6f1cf07fd6bb1527267db
0general <0general@naver.com>
2022-01-11 16:28:11 +0900

    기본적인 정수론

---
+++ baekjoon/카잉 달력.py
@@ -0,0 +1,27 @@
+"""
+https://www.acmicpc.net/problem/6064
+"""
+import math
+import sys
+
+input = sys.stdin.readline
+# year = Ma+x = Nb+y 인 year 찾기
+
+# 연립 일차 합동식 공부해서 다시 해볼 것.
+T = int(input())
+for _ in range(T):
+    m, n, x, y = map(int, input().split())
+    last = (m*n)//math.gcd(m, n)  # LCM 최소공배수이자 마지막 해
+    if m == x and n == y:
+        print(last)
+        continue
+    i = 0
+    num = 0
+    ans = -1
+    while num < last:
+        num = m*i + x
+        if num % n == y % n:
+            ans = num
+            break
+        i += 1
+    print(ans)
