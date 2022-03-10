1d899e8fc69f712a7f8f2c2d5f766dd95b2d525c
0general <0general@naver.com>
2022-03-11 00:23:06 +0900

    행렬곱으로 시간 초과 해결

--- baekjoon/타일 채우기 2.py
+++ baekjoon/타일 채우기 2.py
@@ -6,36 +6,37 @@ n = int(input())
 mod = 1000000007
 
 # f(x) 를 x번째에서 타일이 빠짐없이 채워지는 경우라고 계산하면
-# f(x) = f(x-2)*3 + (x-2에서 한 칸만 채워지는 경우) 라고 할 수 있다. 
-# 이 한 칸만 채워지는 경우를 또 따져보면 f(x-4)에서 2x1 칸을 위아래로 붙이는 두가지 경우에, 
+# f(x) = f(x-2)*3 + (x-2에서 한 칸만 채워지는 경우) 라고 할 수 있다.
+# 이 한 칸만 채워지는 경우를 또 따져보면 f(x-4)에서 2x1 칸을 위아래로 붙이는 두가지 경우에,
 # 다시 x-4에서 한 칸만 채워지는 경우를 더한 것이 된다.
-# 즉 f(x) = f(x-2)*3 + 2*f(x-4) + x-4에서 한 칸만 채워지는 경우 
+# 즉 f(x) = f(x-2)*3 + 2*f(x-4) + x-4에서 한 칸만 채워지는 경우
 # 정리하면 f(x) = 3*f(x-2) + 2{f(x-4) + f(x-6) + f(x-8)+ ... + f(0)} + 0(0에서 한 칸 채워지는 경우)
 # f(x-2) = 3*f(x-4) + 2{f(x-6) + f(x-8) + ... + f(0)} 이므로
 # f(x)-f(x-2) = 3*f(x-2) - f(x-4) 가 된다.
 # 따라서 f(x) = 4*f(x-2) - f(x-4)이다.
 # f(0) = 1, f(2) = 3, 홀수는 불가능
 # 행렬곱을 사용하자
+
+
 def matmul(num):
-    mat = [4,-1,1,0]
-    if num == 0:
-        return [1,0,0,1]
     if num == 1:
-        return mat
+        return [4, -1, 1, 0]
     else:
         M = num//2
         arr = matmul(M)
-        temp = [0,0,0,0]
-        temp[0] = (arr[0]*arr[0] + arr[1]*arr[2] + mod)%mod
-        temp[1] = ((arr[0]+arr[3])*arr[1] + mod)%mod
-        temp[2] = ((arr[0]+arr[3])*arr[2] + mod)%mod
-        temp[3] = (arr[1]*arr[2] + arr[3]*arr[3] + mod)%mod
-        arr = temp
-        if num%2:
-            temp[0] = 
-            temp[1] = 
-            temp
+        temp = [0, 0, 0, 0]
+        temp[0] = (arr[0]*arr[0] + arr[1]*arr[2] + mod) % mod
+        temp[1] = ((arr[0]+arr[3])*arr[1] + mod) % mod
+        temp[2] = ((arr[0]+arr[3])*arr[2] + mod) % mod
+        temp[3] = (arr[1]*arr[2] + arr[3]*arr[3] + mod) % mod
 
+        if num % 2:
+            arr[0] = (4*temp[0] + temp[1] + mod) % mod
+            arr[1] = -temp[0]
+            arr[2] = (4*temp[2] + temp[3] + mod) % mod
+            arr[3] = -temp[2]
+            return arr
+        return temp
 
 
 if n % 2:
@@ -43,7 +44,5 @@ if n % 2:
 elif n == 2:
     print(3)
 else:
-    mat = [4,-1,1,0]
-    ans = [1,0,0,1]
-    
-    
+    mat = matmul(n//2-1)
+    print((mat[0]*3 + mat[1]+mod) % mod)
