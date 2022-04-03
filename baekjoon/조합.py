2d3cb5b9095b3ac49bce9b37b0285142f89e95b3
0general <0general@naver.com>
2022-04-03 23:35:55 +0900

    간단한 dp로 조합

---
+++ baekjoon/조합.py
@@ -0,0 +1,11 @@
+"""
+https://www.acmicpc.net/problem/2407
+"""
+n, m = map(int, input().split())
+
+dp = [1]*(n+1)
+for i in range(2, n+1):
+    dp[i] = dp[i-1]*i
+
+# print(dp[n]//(dp[n-m]*dp[m]))
+print((dp[n]//dp[n-m])//dp[m])
