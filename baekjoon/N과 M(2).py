3f11012f32119658d1cfb17e6d82c0a971c39053
0general <0general@naver.com>
2022-04-30 21:04:51 +0900

    조합 itertools 없이 직접 짜는게 훨씬 빠르고 메모리도 적게 사용한다. 순열과의 차이점은 for문을 사용하지 않아도 된다는 점

--- baekjoon/N과 M(2).py
+++ baekjoon/N과 M(2).py
@@ -1,12 +1,30 @@
 '''
 https://www.acmicpc.net/problem/15650
 '''
-from itertools import combinations
+# from itertools import combinations
+#
+# n, m = map(int, input().split())
+# arr = list(combinations(range(1, n+1), m))
+#
+# for i in arr:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
+
+# itertools 없이 조합 짜기
+import sys
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = list(combinations(range(1, n+1), m))
+arr = [i for i in range(1,n+1)]
 
-for i in arr:
-    for j in i:
-        print(j, end=' ')
-    print()
+def dfs(i,num,string):
+    if num == m:
+        print(string)
+        return
+    if i >= n or n-i < m-num:
+        return
+    dfs(i+1,num+1,string+str(arr[i])+" ")
+    dfs(i+1,num,string)
+
+dfs(0,0,"")
