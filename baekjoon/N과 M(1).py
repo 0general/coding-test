82ec2bbbd93cf20a8b22bfc52b33bf0c4a501000
0general <0general@naver.com>
2022-04-30 20:54:07 +0900

    순열 itertools 없이 직접 짜는게 훨씬 빠르고 메모리도 덜 먹는다.

--- baekjoon/N과 M(1).py
+++ baekjoon/N과 M(1).py
@@ -1,12 +1,33 @@
 '''
 https://www.acmicpc.net/problem/15649
 '''
-from itertools import permutations
+# from itertools import permutations
+#
+# n, m = map(int, input().split())
+# arr = list(permutations(range(1, n+1), m))
+#
+# for i in arr:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
+#
+
+# itertools 없이 순열구하기
+import sys
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = list(permutations(range(1, n+1), m))
+arr = [i for i in range(1,n+1)]
+visited = [False]*n
+def dfs(num,string):
+    if num == m:
+        print(string)
+        return
+    for idx, k in enumerate(arr):
+        if not visited[idx]:
+            visited[idx] = True
+            dfs(num+1,string+str(k)+" ")
+            visited[idx] = False
 
-for i in arr:
-    for j in i:
-        print(j, end=' ')
-    print()
+dfs(0,"")
