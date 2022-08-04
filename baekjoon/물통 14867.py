d333a3f68417e829063b168ad6b88249f1769c33
0general <0general@naver.com>
2022-08-04 12:14:18 +0900

    메모리를 항상 생각하자, 물통 bfs, set으로 방문처리

--- baekjoon/물통 14867.py
+++ baekjoon/물통 14867.py
@@ -1,28 +1,38 @@
 """
 https://www.acmicpc.net/problem/14867
 """
+# 기본 BFS로 43점만 받아 부분 성공 고쳐야 함 -> 메모리 고려 안 함
+# 10만 * 10만 배열로 방문처리를 하면 터진다.
+
 from collections import deque
 
 a, b, c, d = map(int, input().split())
 
-if c == 0 and d == 0:
-    print(0)
-else:
-    visited = [[False]*(b+1) for _ in range(a+1)]
-    ans = -1
-    visited[0][0] = True
-    q = deque()
-    q.append((0, 0, 0))
-    while q:
-        x, y, t = q.popleft()
-        for nx, ny in [(a, y), (x, b), (x, 0), (0, y), (a, x+y-a), (x+y, 0), (x+y-b, b), (0, x+y)]:
-            if 0 <= nx <= a and 0 <= ny <= b and not visited[nx][ny]:
-                visited[nx][ny] = True
-                q.append((nx, ny, t+1))
-                if nx == c and ny == d:
-                    ans = t+1
-                    break
-        else:
-            continue
+visited = set()
+ans = -1
+visited.add((0, 0))
+q = deque()
+q.append((0, 0, 0))
+while q:
+    x, y, t = q.popleft()
+    if x == c and y == d:
+        ans = t
         break
-    print(ans)
+    for nx, ny in [(a, y), (x, b), (x, 0), (0, y)]:
+        if (nx, ny) not in visited:
+            visited.add((nx, ny))
+            q.append((nx, ny, t+1))
+    if a-x >= y and (x+y, 0) not in visited:
+        visited.add((x+y, 0))
+        q.append((x+y, 0, t+1))
+    elif a-x < y and (a, x+y-a) not in visited:
+        visited.add((a, x+y-a))
+        q.append((a, x+y-a, t+1))
+    if b-y >= x and (0, x+y) not in visited:
+        visited.add((0, x+y))
+        q.append((0, x+y, t+1))
+    elif b-y < x and (x+y-b, b) not in visited:
+        visited.add((x+y-b, b))
+        q.append((x+y-b, b, t+1))
+
+print(ans)
