10e6561eb50aab1a0df361fe8466803fc53d4087
0general <0general@naver.com>
2022-09-28 00:17:27 +0900

    dp[4]]에 잘못 썼다가 틀렸던 쉬운 문제

---
+++ baekjoon/돌 게임 3.py
@@ -0,0 +1,14 @@
+"""
+https://www.acmicpc.net/problem/9657
+"""
+dp = ['' for _ in range(1001)]
+dp[1] = 'SK'
+dp[2] = 'CY'
+dp[3] = 'SK'
+dp[4] = 'SK'
+for i in range(5, 1001):
+    dp[i] = 'SK'
+    if dp[i-1] == 'SK' and dp[i-3] == 'SK' and dp[i-4] == 'SK':
+        dp[i] = 'CY'
+
+print(dp[int(input())])
